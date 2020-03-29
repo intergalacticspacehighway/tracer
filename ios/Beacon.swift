82f42aa57053c4b0fc24a6b04ef65f637ff02182
--global <nishanbende@gmail.com>
2020-03-29 09:57:46 +0530

    on broadcast success event iOS

--- ios/Beacon.swift
+++ ios/Beacon.swift
@@ -22,7 +22,7 @@ class Beacon:  RCTEventEmitter,CBPeripheralManagerDelegate,CBCentralManagerDeleg
   var beaconPeripheralData: NSDictionary!
   var peripheralManager: CBPeripheralManager!
   var peripherals:[CBPeripheral] = []
-    var manager: CBCentralManager? = nil
+  var manager: CBCentralManager? = nil
   
   @objc
   override static func requiresMainQueueSetup() -> Bool {
@@ -36,15 +36,15 @@ class Beacon:  RCTEventEmitter,CBPeripheralManagerDelegate,CBCentralManagerDeleg
     
     manager = CBCentralManager(delegate: self, queue: nil)
     
-
+    
     if localBeacon != nil {
-        stopBroadcast()
+      stopBroadcast()
     }
-
+    
     let localBeaconUUID = uuid
     let localBeaconMajor: CLBeaconMajorValue = 123
     let localBeaconMinor: CLBeaconMinorValue = 456
-
+    
     let uuid = UUID(uuidString: localBeaconUUID)!
     localBeacon = CLBeaconRegion(proximityUUID: uuid, major: localBeaconMajor, minor: localBeaconMinor, identifier: "abcd")
     
@@ -56,81 +56,89 @@ class Beacon:  RCTEventEmitter,CBPeripheralManagerDelegate,CBCentralManagerDeleg
   
   @objc
   func stopBroadcast() {
-      peripheralManager.stopAdvertising()
-      peripheralManager = nil
-      beaconPeripheralData = nil
-      localBeacon = nil
+    peripheralManager.stopAdvertising()
+    peripheralManager = nil
+    beaconPeripheralData = nil
+    localBeacon = nil
   }
   
   
   @objc
   func stopScanning() {
     manager?.stopScan();
-      
+    
   }
-
+  
   func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
-      if peripheral.state == .poweredOn {
-        if !peripheralManager.isAdvertising {
-          peripheralManager.startAdvertising(beaconPeripheralData as? [String: Any])
-        }
-      } else if peripheral.state == .poweredOff {
-          peripheralManager.stopAdvertising()
+    if peripheral.state == .poweredOn {
+      if !peripheralManager.isAdvertising {
+        peripheralManager.startAdvertising(beaconPeripheralData as? [String: Any])
       }
+    } else if peripheral.state == .poweredOff {
+      peripheralManager.stopAdvertising()
+    }
   }
-//  centralManagerDidUpdateStat
+  //  centralManagerDidUpdateStat
   func updateDistance(_ distance: CLProximity)
   {
-      
-      print(distance)
+    
+    print(distance)
   }
   func stopScanForBLEDevice(){
-      manager?.stopScan()
-      print("scan stopped")
+    manager?.stopScan()
+    print("scan stopped")
   }
   //CBCentralMaganerDelegate code
   func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
     
-      if (!peripherals.contains(peripheral))
+    if (!peripherals.contains(peripheral))
+    {
+      peripherals.append(peripheral)
+      print(peripheral.state)
+      print(RSSI)
+      print(advertisementData)
+      
+      if let dta = advertisementData["kCBAdvDataManufacturerData"] as? Data
       {
-          peripherals.append(peripheral)
-        print(peripheral.state)
-        print(RSSI)
-        print(advertisementData)
-     
-     if let dta = advertisementData["kCBAdvDataManufacturerData"] as? Data
-        {
-          print(dta.hexEncodedString())
-          let data = dta.hexEncodedString();
-//          let uuid = data.substring(with: Range.)(from: 8 , to:40);
-          let uuid = data[8..<40];
-          
-         
-          print("receiving uuid ", uuid);
-//          let uuid = 004c0215848da4e8079358138a36a57ac46486e800010002c5
-          sendEvent(withName: "onScanResult", body:
+        print(dta.hexEncodedString())
+        let data = dta.hexEncodedString();
+        //          let uuid = data.substring(with: Range.)(from: 8 , to:40);
+        let uuid = data[8..<40];
+        
+        
+        print("receiving uuid ", uuid);
+        //          let uuid = 004c0215848da4e8079358138a36a57ac46486e800010002c5
+        sendEvent(withName: "onScanResult", body:
           [
             
-              "rssi":RSSI,
-              "deviceId":uuid
+            "rssi":RSSI,
+            "deviceId":uuid
             
-          ])
-        }
-        
-        
+        ])
       }
-    
       
-      }
+      
+    }
+    
+  }
+  
+  func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
+    
+    sendEvent(withName: "onBroadcastSuccess", body:
+      [
+        "message":"Broadcast success",
+    ]);
+  }
+  
   func centralManagerDidUpdateState(_ central: CBCentralManager) {
-      print(central.state)
+    print(central.state)
     if central.state == .poweredOn
     {
       if !(central.isScanning) {
         manager?.scanForPeripherals(withServices: nil, options: [CBCentralManagerScanOptionAllowDuplicatesKey : true])
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 60.0) {
-//            self.stopScanForBLEDevice()
-//        }
+        //        DispatchQueue.main.asyncAfter(deadline: .now() + 60.0) {
+        //            self.stopScanForBLEDevice()
+        //        }
       }
       
     }
@@ -140,58 +148,58 @@ class Beacon:  RCTEventEmitter,CBPeripheralManagerDelegate,CBCentralManagerDeleg
   func startScanning(uuid: String) -> Void
   {
     manager?.scanForPeripherals(withServices: nil, options: nil)
-
-//          DispatchQueue.main.asyncAfter(deadline: .now() + 60.0) {
-//              self.stopScanForBLEDevice()
-//          }
-      print("uuid ", uuid)
-   }
-
+    
+    //          DispatchQueue.main.asyncAfter(deadline: .now() + 60.0) {
+    //              self.stopScanForBLEDevice()
+    //          }
+    print("uuid ", uuid)
+  }
+  
   @objc(supportedEvents)
   override public func supportedEvents() -> [String] {
-      return [
-          "onScanResult",
-          "onScanResultBulk",
-          "onScanFailed",
-          "onBroadcastSuccess",
-          "onBroadcastFailure",
-          "onStopScanning",
-          "onStopBroadcast"
-          
-               ]
+    return [
+      "onScanResult",
+      "onScanResultBulk",
+      "onScanFailed",
+      "onBroadcastSuccess",
+      "onBroadcastFailure",
+      "onStopScanning",
+      "onStopBroadcast"
+      
+    ]
   }
 }
 
 extension Data {
-    struct HexEncodingOptions: OptionSet {
-        let rawValue: Int
-        static let upperCase = HexEncodingOptions(rawValue: 1 << 0)
-    }
-
-    func hexEncodedString(options: HexEncodingOptions = []) -> String {
-        let format = options.contains(.upperCase) ? "%02hhX" : "%02hhx"
-        var finalArr = map { String(format: format, $0) }
-      
-//      if finalArr.count > 19
-//      {
-//        finalArr = finalArr.dropLast(5)
-//      }
-      
-      
-        return finalArr.joined()
-    }
+  struct HexEncodingOptions: OptionSet {
+    let rawValue: Int
+    static let upperCase = HexEncodingOptions(rawValue: 1 << 0)
+  }
+  
+  func hexEncodedString(options: HexEncodingOptions = []) -> String {
+    let format = options.contains(.upperCase) ? "%02hhX" : "%02hhx"
+    var finalArr = map { String(format: format, $0) }
+    
+    //      if finalArr.count > 19
+    //      {
+    //        finalArr = finalArr.dropLast(5)
+    //      }
+    
+    
+    return finalArr.joined()
+  }
 }
 
 extension String {
-    subscript(_ range: CountableRange<Int>) -> String {
-        let start = index(startIndex, offsetBy: max(0, range.lowerBound))
-        let end = index(start, offsetBy: min(self.count - range.lowerBound,
-                                             range.upperBound - range.lowerBound))
-        return String(self[start..<end])
-    }
-
-    subscript(_ range: CountablePartialRangeFrom<Int>) -> String {
-        let start = index(startIndex, offsetBy: max(0, range.lowerBound))
-         return String(self[start...])
-    }
+  subscript(_ range: CountableRange<Int>) -> String {
+    let start = index(startIndex, offsetBy: max(0, range.lowerBound))
+    let end = index(start, offsetBy: min(self.count - range.lowerBound,
+                                         range.upperBound - range.lowerBound))
+    return String(self[start..<end])
+  }
+  
+  subscript(_ range: CountablePartialRangeFrom<Int>) -> String {
+    let start = index(startIndex, offsetBy: max(0, range.lowerBound))
+    return String(self[start...])
+  }
 }
